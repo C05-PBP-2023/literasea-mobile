57391a8c8f60ef355d3689cfc11b8a0e5cace4e3
nabilmuafa <nmuafa@gmail.com>
2023-12-08 22:56:57 +0700

    create rough home page

--- lib/json/const.dart
+++ lib/json/const.dart
@@ -6,3 +6,9 @@ List bottomNavBarIcons = [
   {"icon": Icons.history, "name": "Order History"},
   {"icon": Icons.account_circle, "name": "Account"}
 ];
+
+List homePageButtons = [
+  {"icon": Icons.shopping_basket_outlined, "name": "Catalogue"},
+  {"icon": Icons.create_rounded, "name": "Reviews"},
+  {"icon": Icons.question_answer, "name": "Q & A"},
+];
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:literasea_mobile/screens/home_page.dart';
+import 'package:literasea_mobile/screens/root_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +12,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: MyHomePage(title: "Literasea"));
+        debugShowCheckedModeBanner: false, home: RootPage());
   }
 }
