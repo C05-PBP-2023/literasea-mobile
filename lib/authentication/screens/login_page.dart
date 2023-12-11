e742eab375d8c3d25710f089d6cc0a91bc6a229f
codaaa19 <tsabitiphone@gmail.com>
2023-12-11 14:08:53 +0700

    delete unnecessary lines

--- lib/authentication/screens/login_page.dart
+++ lib/authentication/screens/login_page.dart
@@ -39,62 +39,6 @@ class _LoginPageState extends State<LoginPage> {
       body: SafeArea(
         child: Stack(
           children: [
-            TextField(
-              controller: _usernameController,
-              decoration: const InputDecoration(
-                labelText: 'Username',
-              ),
-            ),
-            const SizedBox(height: 12.0),
-            TextField(
-              controller: _passwordController,
-              decoration: const InputDecoration(
-                labelText: 'Password',
-              ),
-              obscureText: true,
-            ),
-            const SizedBox(height: 24.0),
-            ElevatedButton(
-              onPressed: () async {
-                final navigator = Navigator.of(context);
-                String username = _usernameController.text;
-                String password = _passwordController.text;
-                final response = await request
-                    .login("https://literasea.live/auth/login-mobile/", {
-                  'username': username,
-                  'password': password,
-                });
-
-                if (request.loggedIn) {
-                  String message = response['message'];
-                  String uname = response['username'];
-                  await navigator.pushAndRemoveUntil(
-                      MaterialPageRoute(builder: (context) => const RootPage()),
-                      (route) => false);
-                  ScaffoldMessenger.of(context)
-                    ..hideCurrentSnackBar()
-                    ..showSnackBar(SnackBar(
-                        content: Text("$message Selamat datang, $uname.")));
-                } else {
-                  showDialog(
-                    context: context,
-                    builder: (context) => AlertDialog(
-                      title: const Text('Login Gagal'),
-                      content: Text(response['message']),
-                      actions: [
-                        TextButton(
-                          child: const Text('OK'),
-                          onPressed: () {
-                            navigator.pop();
-                          },
-                        ),
-                      ],
-                    ),
-                  );
-                }
-              },
-              child: const Text('Login'),
-            ),
             Container(
               decoration: const BoxDecoration(
                   gradient: LinearGradient(
